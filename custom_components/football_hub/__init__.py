086b03352bca6300ee213fdf0353b2275f109c80
Ady <94404576+Adya84@users.noreply.github.com>
2026-06-28 22:39:28 +0100

    Update __init__.py

--- custom_components/football_hub/__init__.py
+++ custom_components/football_hub/__init__.py
@@ -1,6 +1,7 @@
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 
+from .api.coordinator import FootballHubCoordinator
 from .const import DOMAIN
 
 PLATFORMS = ["sensor"]
@@ -13,7 +14,13 @@ async def async_setup(hass: HomeAssistant, config: dict):
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     hass.data.setdefault(DOMAIN, {})
-    hass.data[DOMAIN][entry.entry_id] = {}
+
+    coordinator = FootballHubCoordinator(hass, entry)
+    await coordinator.async_config_entry_first_refresh()
+
+    hass.data[DOMAIN][entry.entry_id] = {
+        "coordinator": coordinator,
+    }
 
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
