35fe716e1890e6ac9628409c140d5175f9fc43c4
Ady <94404576+Adya84@users.noreply.github.com>
2026-07-03 11:23:48 +0100

    Update __init__.py

--- custom_components/football_hub/engine/__init__.py
+++ custom_components/football_hub/engine/__init__.py
@@ -1,1 +1,7 @@
+"""Football Hub engine exports."""
 
+from .fixtures import fixtures_count, next_fixture, upcoming_fixtures
+from .live import live_count, live_matches
+from .players import top_assists, top_scorers
+from .results import last_result, results, results_count
+from .standings import standings, standings_count
